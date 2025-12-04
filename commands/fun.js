1b119dac219f7519a7d6331a50e2c3509f15891d
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-04 16:39:47 +0530

    Create fun.js

---
+++ commands/fun.js
@@ -0,0 +1,9 @@
+// commands/fun.js
+
+module.exports = { 
+    hack: async(c,m,a) => m.reply("😂😂 Target HACKED (Just kidding!) 😂😂"), 
+    virus: async(c,m,a) => m.reply("Fun: Virus Simulation Logic"), 
+    whois: async(c,m,a) => m.reply("Fun: Whois Logic"), 
+    meme: async(c,m,a) => m.reply("Fun: Meme Logic"), 
+    joke: async(c,m,a) => m.reply("Fun: Joke Logic") 
+};
