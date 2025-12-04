3de96121366f4b4176211194bbc50163a1aaa88b
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-04 16:39:00 +0530

    Create graphics.js

---
+++ commands/graphics.js
@@ -0,0 +1,9 @@
+// commands/graphics.js
+
+module.exports = { 
+    hackerlogo: async(c,m,a) => m.reply("Graphics: Hacker Logo Logic"), 
+    neon: async(c,m,a) => m.reply("Graphics: Neon Logo Logic"), 
+    matrix: async(c,m,a) => m.reply("Graphics: Matrix Logo Logic"), 
+    firetext: async(c,m,a) => m.reply("Graphics: Firetext Logo Logic"), 
+    dark: async(c,m,a) => m.reply("Graphics: Dark Logo Logic") 
+};
