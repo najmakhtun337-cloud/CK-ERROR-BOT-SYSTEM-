8d8052c67376c2e2ae4bd73c3e05a09f88a44315
DARK-ERRO <najmakhtun337@gmail.com>
2025-12-04 16:18:24 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,31 @@
+{
+  "name": "ck-error-bot-system",
+  "version": "1.0.0",
+  "description": "A powerful WhatsApp bot system ready for Render deployment.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "dev": "nodemon index.js"
+  },
+  "keywords": [
+    "whatsapp-bot",
+    "node.js",
+    "wwebjs",
+    "render",
+    "ck-error"
+  ],
+  "author": "CK-A ERROR",
+  "license": "Apache-2.0",
+  "dependencies": {
+    "whatsapp-web.js": "^1.23.0",
+    "qrcode-terminal": "^0.12.0",
+    "dotenv": "^16.3.1",
+    "axios": "^1.6.2",
+    "ytdl-core": "^4.11.5",
+    "fluent-ffmpeg": "^2.1.2",
+    "fs": "^0.0.1-security" 
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
